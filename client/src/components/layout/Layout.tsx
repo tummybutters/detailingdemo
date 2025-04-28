cb7e87735ed529241b752f105689db7ed44e7136
tommyboybutcher <39116111-tommyboybutcher@users.noreply.replit.com>
2025-04-28 16:10:50 +0000

    Restored to '349762057983bafcb9636a917a7e1c3ecc50cf13'
    Replit-Restored-To: 349762057983bafcb9636a917a7e1c3ecc50cf13

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import DiscountModal from "../marketing/DiscountModal";
 
 interface LayoutProps {
   children: ReactNode;
@@ -13,7 +12,6 @@ export default function Layout({ children }: LayoutProps) {
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
-      <DiscountModal />
     </div>
   );
 }
